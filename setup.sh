54e308ab75737d1d8046951b86900e39b3a7d1c3
zediogocastro <83618323+zediogocastro@users.noreply.github.com>
2024-09-30 16:12:50 +0000

    adds shell scripting

---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+run_containers() {
+    echo "Starting the containers..."
+    docker-compose up -d
+    echo "Containers are up and running!"
+    # Step 2: Run the database setup and ETL scripts
+    docker-compose exec web python create_tables.py
+    docker-compose exec web python etl.py
+
+}
+
+list_containers() {
+    echo "Listing running containers..."
+    docker ps  # Show the running Docker containers
+}
+
+stop_containers() {
+    echo "Stopping the containers..."
+    docker-compose down  # Stop all running containers
+    echo "Containers have been stopped."
+}
+
+# Parse the command-line arguments
+if [ "$1" == "--run" ]; then
+    run_containers
+elif [ "$1" == "--top" ]; then
+    list_containers
+elif [ "$1" == "--stop" ]; then
+    stop_containers
+else
+    echo "Invalid argument. Use one of the following:"
+    echo "--run   : Run containers"
+    echo "--top   : List running containers"
+    echo "--stop  : Stop running containers"
+fi
